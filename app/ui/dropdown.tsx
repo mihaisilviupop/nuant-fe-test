af7b7630ed2218886530604a90b92313a7361c32
mihaisilviupop <mihai.silviu.pop@gmail.com>
2024-04-26 01:56:03 +0300

    feat: added dropdown component

---
+++ app/ui/dropdown.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+export default function Dropdown(
+    {
+        list,
+        placeholder,
+    }: {
+        list: string[];
+        placeholder: string;
+    }
+) {
+    const searchParams = useSearchParams();
+    const { replace } = useRouter();
+    const pathname = usePathname();
+
+    const handleSelect = (term: string) => {
+        const params = new URLSearchParams(searchParams);
+        if (term) {
+            params.set('type', term);
+        } else {
+            params.delete('type');
+        }
+
+        replace(`${pathname}?${params.toString()}`);
+    };
+
+    return (
+        <form className="max-w-sm mx-auto">
+            <select
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={(e) => {
+                    handleSelect(e.target.value);
+                }}
+                defaultValue={searchParams.get('type')?.toString() ?? 'all'}
+            >
+                <option value="all">{placeholder}</option>
+                {
+                    list.map((name, index) => (
+                        <option
+                            key={index}
+                            value={name}
+                        >
+                            {name}
+                        </option>
+                    ))
+                }
+            </select>
+        </form >
+    );
+}
