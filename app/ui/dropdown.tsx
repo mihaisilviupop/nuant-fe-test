c59f8ba7efc92d426a3f498d4c2e6d7c89036056
mihaisilviupop <mihai.silviu.pop@gmail.com>
2024-04-26 23:58:30 +0300

    fix: typo in placeholder, capitalize types

--- app/ui/dropdown.tsx
+++ app/ui/dropdown.tsx
@@ -29,7 +29,7 @@ export default function Dropdown(
     return (
         <form className="max-w-sm w-1/3">
             <select
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 capitalize"
                 onChange={(e) => {
                     handleSelect(e.target.value);
                 }}
