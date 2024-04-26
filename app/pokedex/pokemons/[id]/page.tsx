43e17fb0a6cf01d2a338a41042f609e5616e0e37
mihaisilviupop <mihai.silviu.pop@gmail.com>
2024-04-27 01:17:55 +0300

    feat: added image to pokemon details

--- app/pokedex/pokemons/[id]/page.tsx
+++ app/pokedex/pokemons/[id]/page.tsx
@@ -1,10 +1,12 @@
 import { getPokemonById } from '@/app/lib/data';
-import { convertDmToCm, convertHgToKg } from '@/app/lib/utils';
+import { convertDmToCm, convertHgToKg, getPokemonImage } from '@/app/lib/utils';
 import Description from '@/app/ui/pokemonDetails/description';
 import DescriptionDetail from '@/app/ui/pokemonDetails/description-detail';
+import Image from 'next/image';
 
 export default async function Page({ params }: { params: { id: string } }) {
     const pokemon = await getPokemonById(params.id);
+    const imageUrl = getPokemonImage(pokemon.sprites);
     return (
         <div className="w-full">
             <h1 className='p-4 text-2xl capitalize'>{pokemon.name}</h1>
@@ -12,7 +14,13 @@ export default async function Page({ params }: { params: { id: string } }) {
                 <DescriptionDetail term='Weight' details={`${convertHgToKg(pokemon.weight)} kg`} />
                 <DescriptionDetail term='Height' details={`${convertDmToCm(pokemon.height)} cm`} />
             </Description>
-
+            <Image
+                src={imageUrl}
+                alt={`${pokemon.name} profile picture`}
+                width={250}
+                height={250}
+                className="rounded-full w-96 h-96"
+            ></Image>
         </div>
     );
 }
