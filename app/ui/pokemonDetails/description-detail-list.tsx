8f9389ead48a289dc84cb1eca1d63c1a571c9563
mihaisilviupop <mihai.silviu.pop@gmail.com>
2024-04-27 02:10:47 +0300

    feat: added desc details list component

---
+++ app/ui/pokemonDetails/description-detail-list.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+export default function DescriptionDetailList({
+    term,
+    details
+}: {
+    term: string;
+    details: string[];
+}) {
+
+    return (
+        <div className="flex flex-col pb-3">
+            <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">{term}</dt>
+            <dd className="text-lg font-semibold grow">
+                {details.map((name, index) => (
+                    <span
+                        key={index}
+                        className='bg-blue-100 text-blue-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300'
+                    >
+                        {name}
+                    </span>
+                ))}
+            </dd>
+        </div>
+    );
+}
