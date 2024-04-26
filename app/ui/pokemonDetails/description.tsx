c8779fad03e6ee5938bfb3a87f7556616b0b66c9
mihaisilviupop <mihai.silviu.pop@gmail.com>
2024-04-27 00:45:13 +0300

    feat: added description component

---
+++ app/ui/pokemonDetails/description.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react';
+
+export default function Description({ children }: { children: ReactNode; }) {
+    return (
+        <dl className="max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
+            {children}
+        </dl>
+    );
+}
