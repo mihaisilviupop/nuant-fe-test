b4780c2df9a62de627ea9319f57a0157546f67e6
mihaisilviupop <mihai.silviu.pop@gmail.com>
2024-04-27 00:46:54 +0300

    feat: use pokemon details description components, style fixes

--- app/pokedex/pokemons/[id]/page.tsx
+++ app/pokedex/pokemons/[id]/page.tsx
@@ -1,13 +1,18 @@
 import { getPokemonById } from '@/app/lib/data';
+import { convertDmToCm, convertHgToKg } from '@/app/lib/utils';
+import Description from '@/app/ui/pokemonDetails/description';
+import DescriptionDetail from '@/app/ui/pokemonDetails/description-detail';
 
 export default async function Page({ params }: { params: { id: string } }) {
     const pokemon = await getPokemonById(params.id);
     return (
-        <>
-            <div>{params.id}</div>
-            <div>{pokemon.name}</div>
-            <div>{pokemon.weight}</div>
-            <div>{pokemon.height}</div>
-        </>
+        <div className="w-full">
+            <h1 className='p-4 text-2xl capitalize'>{pokemon.name}</h1>
+            <Description>
+                <DescriptionDetail term='Weight' details={`${convertHgToKg(pokemon.weight)} kg`} />
+                <DescriptionDetail term='Height' details={`${convertDmToCm(pokemon.height)} cm`} />
+            </Description>
+
+        </div>
     );
 }
