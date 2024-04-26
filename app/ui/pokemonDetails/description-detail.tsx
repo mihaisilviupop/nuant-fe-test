d7288c47fdb19c71afa226b357bd890a74485dda
mihaisilviupop <mihai.silviu.pop@gmail.com>
2024-04-27 01:40:09 +0300

    feat: add url to description details component

--- app/ui/pokemonDetails/description-detail.tsx
+++ app/ui/pokemonDetails/description-detail.tsx
@@ -1,15 +1,21 @@
+import Link from 'next/link';
+
 export default function DescriptionDetail({
     term,
     details,
+    url
 }: {
     term: string;
     details: string;
+    url?: string
 }) {
 
     return (
         <div className="flex flex-col pb-3">
             <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">{term}</dt>
-            <dd className="text-lg font-semibold grow">{details}</dd>
+            <dd className="text-lg font-semibold grow">
+                {url ? <Link href={`${url}`}>{details}</Link> : details}
+            </dd>
         </div>
     );
 }
